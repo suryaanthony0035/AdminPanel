1f509c5d1a513d01cda9895143ec7fc42f5f1d12
suryaanthony0035 <exdrs.emp.0035.surya@gmail.com>
2022-06-17 13:30:16 +0530

    test

--- lib/style/colors.dart
+++ lib/style/colors.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 const primaryColor = Color(0xFF2697FF);
 const secondaryColor = Color(0xFF2A2D3E);
 // const secondaryColor = Color(0xFFEEEEEE);
-// const bgColor = Color(0xFF212332);
+// const test bgColor = Color(0xFF212332);
 const bgColor = Color(0xFF009688);
 
 const defaultPadding = 16.0;
